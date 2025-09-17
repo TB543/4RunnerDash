2892aabb3acde9b55b4d554199d87d14f69b542e
Ty <92407605+TB543@users.noreply.github.com>
2025-09-17 11:08:01 -0400

    return to parent directory on start

--- start_backend.sh
+++ start_backend.sh
@@ -14,3 +14,4 @@ bluetoothctl pairable on
 nohup ../venv/bin/nominatim serve > /dev/null 2>&1 &
 nohup java -jar Lib/graphhopper.jar server ../resources/graphhopper_config.yml > /dev/null 2>&1 &
 sudo docker run -d --name tileserver -v $(pwd)/Lib/tileserver:/data -v $(pwd)/AppData/map_data:/data/map_data -p 8080:8080 maptiler/tileserver-gl:latest
+cd ..
